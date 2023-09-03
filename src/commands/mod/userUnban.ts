34bc9e016847499627d9c63a1b46a4a89d09e1c0
smnski <szymonurbanski217@gmail.com>
2023-09-03 22:37:03 +0200

    unbanning user works

---
+++ src/commands/mod/userUnban.ts
@@ -0,0 +1,44 @@
+import { CommandInteraction } from "oceanic.js";
+import { ISlvtCommand } from "../../bot";
+import { Users } from "../../databases/user/models";
+
+const modUserUnbanCommand: ISlvtCommand = {
+    data: {
+        type: 1,
+        name: "mod_unban_user",
+        description: "Lifts the ban on given user.",
+        defaultMemberPermissions: "4",
+        options: [
+            {
+                type: 6,
+                name: "user",
+                description: "User to unban.",
+                required: true
+            }
+        ]
+    },
+    async execute(interaction: CommandInteraction) {
+        
+        const user_option = interaction.data.options.getUser("user")!;
+        const user_id = user_option.id;
+        const guild_id = interaction.guildID!;
+        const field = ['anon_banned'];
+        const value = [false];
+
+        try {
+            const changed_user = await Users.changeUser(user_id, guild_id, field, value);
+            if(!changed_user) {
+                await interaction.createMessage({ content: "User wasn't found.", flags: 64});
+                return;
+            }
+
+        } catch(error) {
+            console.log(error);
+            await interaction.createMessage({ content: "Something went wrong while interacting with the database.", flags: 64 });
+            return;
+        }
+        await interaction.createMessage({ content: `${user_option.username} was successfully unbanned.`, flags: 64});
+    }
+}
+
+export default modUserUnbanCommand;
